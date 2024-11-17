56e0a5b191fb74752590ef86a5d97893dd273a20
Posh <p0sh@proton.me>
2024-11-17 13:32:45 +0000

    Add MudBlazor

--- src/E621Maui.UI/MauiProgram.cs
+++ src/E621Maui.UI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace E621Maui.UI
 {
@@ -15,6 +16,7 @@ namespace E621Maui.UI
                 });
 
             builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
 
 #if DEBUG
     		builder.Services.AddBlazorWebViewDeveloperTools();
